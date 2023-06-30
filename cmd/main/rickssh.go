0b91d68ff7d4618d107fd4f25e8737c3fbf1a536
Richard Kirk <richard.kirk790@gmail.com>
2023-06-30 12:01:15 -0500

    Added Logrus logging and enabled debug level
    This commit adds the Logrus package for leveled logging. The commit also
    adds some logging lines for our connection handler function to test that
    the logging levels work. The commit also contains some logrus
    configuration to enable logging debug logs and removing truncation of
    levels to the console.

--- cmd/main/rickssh.go
+++ cmd/main/rickssh.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"net"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/ulrokx/rickssh/pkg/transport"
 )
 
 func main() {
+	log.SetLevel(log.DebugLevel)
+	log.SetFormatter(&log.TextFormatter{
+		DisableLevelTruncation: true,
+	})
 	listener, err := net.Listen("tcp", ":2222")
 	if err != nil {
 		panic(err)
--- pkg/transport/connection.go
+++ pkg/transport/connection.go
@@ -3,17 +3,20 @@ package transport
 import (
 	"fmt"
 	"net"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func HandleConnection(conn net.Conn, i int) {
-	fmt.Printf("connection %d start\n", i)
+	log.Infof("handing connection # %d from %s", i, conn.RemoteAddr().String())
 	buf := make([]byte, 1024)
 	for {
 		len, err := conn.Read(buf)
 		if err != nil {
-			fmt.Println(fmt.Sprintf("connection %d ended: %v", i, err))
+			log.Infof("connection %d ended: %v", i, err)
 			break
 		}
+		log.Debugf("read %d bytes from connection # %d", len, i)
 		conn.Write([]byte(fmt.Sprintf("%d %s", i, buf[:len])))
 	}
 }
