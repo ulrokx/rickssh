3bbab20723650f0cc5b6988aea6b81766eb65edc
Richard Kirk <richard.kirk790@gmail.com>
2023-06-30 21:00:52 -0500

    Add send and validate protocol exchange
    This commit adds the first step of the SSH2 protocol. We can now send an
    identification string for SSH2 and receive one and validate it.
    
    This file might get pretty big, so it is probably prudent to break off
    this connection package into more files for each step in the
    authentication process.
    
    We will also start storing state for each connection, such as the MAC
    algorithm and any key we need to do kex or ciphers. It would also be
    nice to populate it with any (perhaps useful at some stage) data we
    receive, so figuring out some sort of shared struct for that would be
    ideal.
    
    At this point, an OpenSSH client can connect to our server and get to
    the key exchange process!

--- pkg/transport/connection.go
+++ pkg/transport/connection.go
@@ -1,23 +1,69 @@
 package transport
 
 import (
-	"fmt"
 	"net"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 )
 
+const PROTOCOL_VERSION = "SSH-2.0-rickssh_0.0.1"
+
 func HandleConnection(conn net.Conn, i int) {
 	log.Infof("handing connection # %d from %s", i, conn.RemoteAddr().String())
-	buf := make([]byte, 1024)
-	for {
-		len, err := conn.Read(buf)
-		if err != nil {
-			log.Infof("connection %d ended: %v", i, err)
-			break
+	defer func() {
+		if r := recover(); r != nil {
+			conn.Close()
+			log.Infof("connection # %d ended: %v", i, r)
 		}
-		log.Debugf("read %d bytes from connection # %d", len, i)
-		log.Tracef("connection # %d: read %s", i, buf[:len])
-		conn.Write([]byte(fmt.Sprintf("%d %s", i, buf[:len])))
+	}()
+	sendProtocolVersionExchange(conn, i)
+	receieveProtocolVersionExchange(conn, i)
+	buf := make([]byte, 32565)
+	conn.Read(buf)
+	log.Tracef("connection # %d: read %s", i, buf)
+	for {
+
 	}
+}
+
+func sendProtocolVersionExchange(conn net.Conn, i int) {
+	log.Debugf("connection # %d: sending protocol version exchange: %s\n", i, PROTOCOL_VERSION)
+	_, err := conn.Write([]byte(PROTOCOL_VERSION + "\r\n"))
+	if err != nil {
+		log.Infof("connection ended: %v", err)
+		panic(err)
+	}
+}
+
+func receieveProtocolVersionExchange(conn net.Conn, i int) {
+	buf := make([]byte, 255)
+	length, err := conn.Read(buf)
+	log.Tracef("connection # %d: read %s", i, buf[:length])
+	if err != nil {
+		log.Debugf("connection ended: %v", err)
+		panic(err)
+	}
+	id := string(buf[:length])
+	versionSpaceComments := strings.Split(id, " ")
+	if len(versionSpaceComments) != 2 {
+		log.Debugf("error splitting on space: %s", id)
+		panic("invalid protocol version exchange")
+	}
+	protocolVersionSoftwareVersion := strings.Split(versionSpaceComments[0], "-")
+	if len(protocolVersionSoftwareVersion) != 3 {
+		log.Debugf("error splitting on dash: %s", id)
+		panic("invalid protocol version exchange")
+	}
+	if protocolVersionSoftwareVersion[0] != "SSH" {
+		log.Debugf("error ssh not found: %s", id)
+		panic("invalid protocol version exchange")
+	}
+	if protocolVersionSoftwareVersion[1] != "2.0" {
+		log.Debugf("error version incorrect: %s", id)
+		panic("invalid protocol version exchange")
+	}
+	softwareVersion := protocolVersionSoftwareVersion[2]
+	comments := versionSpaceComments[1]
+	log.Debugf("connection # %d: software version: %s comments: %s", i, softwareVersion, comments)
 }
