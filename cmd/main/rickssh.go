c5e5ce3db9e3131c794f291f39f7f3bc55b0cef7
Richard Kirk <richard.kirk790@gmail.com>
2023-06-30 19:35:34 -0500

    Add debug logging flag
    This commit adds a flag when calling the program '-d' that will enable
    logging logs of level debug.

--- cmd/main/rickssh.go
+++ cmd/main/rickssh.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 
@@ -9,10 +10,11 @@ import (
 )
 
 func main() {
-	log.SetLevel(log.DebugLevel)
-	log.SetFormatter(&log.TextFormatter{
-		DisableLevelTruncation: true,
-	})
+	debug := flag.Bool("d", false, "enable debug logging")
+	flag.Parse()
+	if *debug {
+		debugMode()
+	}
 	listener, err := net.Listen("tcp", ":2222")
 	if err != nil {
 		panic(err)
@@ -25,4 +27,11 @@ func main() {
 		}
 		go transport.HandleConnection(conn, i)
 	}
+}
+
+func debugMode() {
+	log.SetLevel(log.DebugLevel)
+	log.SetFormatter(&log.TextFormatter{
+		DisableLevelTruncation: true,
+	})
 }
